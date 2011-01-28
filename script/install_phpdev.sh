6bf88860c40721fb8688b3346a8f0af94e7d3a84
Tsuyoshi Saito <tsuyoshi310@gmail.com>
2011-01-28 10:15:53 +0900

    Added php dev stuff installer

---
+++ script/install_phpdev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Installing phpmd
+pear channel-discover pear.phpmd.org
+pear channel-discover pear.pdepend.org
+pear install --alldeps phpmd/PHP_PMD-alpha
+
+# Installing PHPUnit3
+pear channel-discover pear.phpunit.de
+pear channel-discover components.ez.no
+pear channel-discover pear.symfony-project.com
+pear install --alldeps phpunit/PHPUnit
+
+# Installing Ethna
+pear channel-discover pear.ethna.jp
+pear install --alldeps ethna/ethna
