a76a5a9c0b5638515d162023eccb4b990d8dfa13
Tsuyoshi Saito <tsuyoshi.saito@enigmo.co.jp>
2018-04-13 11:48:41 +0900

    cleanup env/path setup

--- home/.config/zsh/export.zsh
+++ home/.config/zsh/export.zsh
@@ -1,5 +1,4 @@
 # vim:set ft=sh :
-
 export LANG=ja_JP.UTF-8
 
 export EDITOR='vim'
@@ -10,20 +9,7 @@ export SVN_EDITOR='vim'
 if [[ $IS_MAC -eq 1 ]]; then
   # Prevent double file (._) file on OSX
   export COPYFILE_DISABLE=true
-
-  # Mac OS X uses path_helper and /etc/paths.d to preload PATH, clear it out first
-  # if [ -x /usr/libexec/path_helper ]; then
-  #   PATH=''
-  #   eval `/usr/libexec/path_helper -s`
-  # fi
 fi
-
-export GOPATH=$HOME/.go
-export JAVA_HOME=$(/usr/libexec/java_home)
-# export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH
-# export PATH=$HOME/pear:$PATH
-# export PATH=$PATH:$HOME/.bin
-# export PATH=$PATH:$GOPATH/bin
 
 export TERM=xterm-256color
 export CLICOLOR=1
@@ -33,24 +19,18 @@ export GREP_COLOR='3;33'
 export LESS='--ignore-case --raw-control-chars'
 export PAGER='less'
 
+export GOPATH=$HOME/.go
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH
+export PATH=$PATH:$GOPATH/bin
+
 if which fasd >/dev/null 2>&1; then
   eval "$(fasd --init auto)"
 fi
 
-if [ -d "$HOME/.nodebrew/current/bin" ]; then
-  export PATH=$HOME/.nodebrew/current/bin:$PATH
-fi
-
 if which direnv > /dev/null 2>&1; then
-  #eval "$(direnv hook zsh)"
-fi
-
-if which pyenv > /dev/null 2>&1; then
-  #eval "$(pyenv init -)"
-fi
-
-if [ -d $HOME/Library/Android/sdk/platform-tools ]; then
-  #export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
+  eval "$(direnv hook zsh)"
 fi
 
 export PYTHONSTARTUP=~/.pythonrc
--- home/.zshenv
+++ home/.zshenv
@@ -1,3 +1,5 @@
 #zmodload zsh/zprof && zprof
 source $HOME/.config/zsh/export.zsh
 source $HOME/.config/zsh/ruby.zsh
+
+[ -f ~/.zshenv.local ] && source ~/.zshenv.local
