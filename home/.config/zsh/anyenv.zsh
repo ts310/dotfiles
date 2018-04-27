ff6b5f56efbf91afe985de8f2cebfb6a88912d10
Tsuyoshi Saito <tsuyoshi.saito@enigmo.co.jp>
2018-04-27 13:43:07 +0900

    zsh optimization

--- home/.config/zsh/anyenv.zsh
+++ home/.config/zsh/anyenv.zsh
@@ -1,11 +1,38 @@
 # anyenv
-if [ -d ${HOME}/.anyenv ]; then
+ANYENV_ROOT="$HOME/.anyenv"
+if [ -d $ANYENV_ROOT ]; then
   export PATH="$HOME/.anyenv/bin:$PATH"
-
-  eval "$(anyenv init -)"
-
+  # eval "$(anyenv init - --no-rehash)"
   for D in `ls $HOME/.anyenv/envs`
   do
     export PATH="$HOME/.anyenv/envs/$D/shims:$PATH"
   done
 fi
+
+function anyenv_init() {
+  eval "$(anyenv init - --no-rehash)"
+}
+
+function anyenv_unset() {
+  unset -f ndenv
+  unset -f rbenv
+  unset -f pyenv
+}
+
+function ndenv() {
+  anyenv_unset
+  anyenv_init
+  ndenv "$@"
+}
+
+function rbenv() {
+  anyenv_unset
+  anyenv_init
+  rbenv "$@"
+}
+
+function pyenv() {
+  anyenv_unset
+  anyenv_init
+  pyenv "$@"
+}
--- home/.config/zsh/prompt.zsh
+++ home/.config/zsh/prompt.zsh
@@ -1,1 +1,2 @@
-source $HOME/.liquidprompt/liquidprompt
+#source $HOME/.liquidprompt/liquidprompt
+PROMPT='%{${fg[cyan]}%}%~:%{${reset_color}%} '
