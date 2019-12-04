84b97a75c8238c77b7959a9b4cf7326a09f053e2
Tsuyoshi Saito <tsuyoshi.saito@enigmo.co.jp>
2019-12-04 20:02:00 +0900

    karabiner setup: emacs binding updated

--- home/.config/zsh/export.zsh
+++ home/.config/zsh/export.zsh
@@ -14,8 +14,6 @@ fi
 export TERM=xterm-256color
 export CLICOLOR=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='3;33'
 export LESS='--ignore-case --raw-control-chars'
 export PAGER='less'
 
