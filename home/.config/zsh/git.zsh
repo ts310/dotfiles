55bf589f2dd9ed39056cf9eebefb9c08f447eb2a
Tsuyoshi Saito <tsuyoshi.saito@enigmo.co.jp>
2020-03-22 11:29:42 +0900

    Update git aliases

--- home/.config/zsh/git.zsh
+++ home/.config/zsh/git.zsh
@@ -6,3 +6,6 @@ git config --global color.branch auto
 git config --global alias.co "checkout"
 git config --global alias.br "branch"
 git config --global alias.ci "commit"
+git config --global alias.fp "fetch --tags --prune"
+git config --global alias.pr "pull --rebase"
+git config --global alias.difff "diff --word-diff"
